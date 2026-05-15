f2535f10e62db12e2357c7febb4cad44127b8b55
jflpro <jf.langlois.pro@gmail.com>
2026-05-15 16:40:41 +0200

    reduce services background image to 50% size

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -37,8 +37,8 @@ export default function Services() {
       aria-labelledby="services-heading"
     >
       <div
-        className="absolute inset-0 bg-center bg-cover"
-        style={{ backgroundImage: "url('/images/services-bg.png')", opacity: 0.2 }}
+        className="absolute inset-0 bg-center"
+        style={{ backgroundImage: "url('/images/services-bg.png')", opacity: 0.2, backgroundSize: '50%' }}
       />
       <div className="max-w-6xl mx-auto px-6 md:px-12 relative z-10">
         {/* En-tête */}
